66d355f75049a3c2fa5b0cfec0261f86c85368ee
emmanuel687 <emmanuel.koech@student.moringaschool.com>
2022-02-19 17:30:15 +0300

    Method added to class Credential

--- credential.py
+++ credential.py
@@ -1,3 +1,8 @@
 class Credential:
     pass
     credentials_array = []
+
+    def __init__(self,user_name,password,email):
+        self.user_name = user_name
+        self.password = password
+        self.email = email

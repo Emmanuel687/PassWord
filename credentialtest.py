1d384be59993dbd807b3e57034900f6d5d7eca09
emmanuel687 <emmanuel.koech@student.moringaschool.com>
2022-02-19 17:44:51 +0300

    credential object add

--- credentialtest.py
+++ credentialtest.py
@@ -1,3 +1,8 @@
 import unittest
 from credential import Credential
 
+class TestUser(unittest.TestCase):
+
+    def setUp(self):
+
+        self.new_credential = Credential("user_name", "password","email@gmail.com")

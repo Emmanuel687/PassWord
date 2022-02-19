d7ba09260fbf40d31e1c06c50300309abc416bd6
emmanuel687 <emmanuel.koech@student.moringaschool.com>
2022-02-19 17:33:55 +0300

    created function for saving user credentials

--- credential.py
+++ credential.py
@@ -2,7 +2,13 @@ class Credential:
     pass
     credentials_array = []
 
-    def __init__(self,user_name,password,email):
-        self.user_name = user_name
+    def __init__(self,userName,password,email):
+        self.userName = userName
         self.password = password
         self.email = email
+    """
+    function for saving user credentials
+    """
+    def save_credentials(self):
+
+        Credential.credentials_array.append(self)

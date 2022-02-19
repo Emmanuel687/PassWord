b8538bd019d5aa772ccd92f86b5e40684fe0c12d
emmanuel687 <emmanuel.koech@student.moringaschool.com>
2022-02-19 18:16:21 +0300

    generated function that generates username in run.py

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 from credential import Credential
 from user import User
+import random
 
+
+def create_user(fullname,email,mobilenumber):
+    """
+    This function generates username
+    """
+
+    new_user = User(fullname,email,mobilenumber)
+    return new_user

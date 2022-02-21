0d782c89e238c5f20bb372d13d740cd130aa633f
emmanuel687 <emmanuel.koech@student.moringaschool.com>
2022-02-21 10:13:50 +0300

    comment  out users specification

--- run.py
+++ run.py
@@ -53,7 +53,12 @@ def display_cred():
     This function that returns saved user credentials
     """
     return Credential.display_credential()
-
+# As a user, I want to create a password locker account with my details, a login username and password.
+# As a user, I want to store my already existing account credentials in the application. Assuming I already have a twitter account, I want to store my already existing twitter username and password in the application.
+# As a user, I want to create new account credentials in the application. For example, if I have not yet signed up for Instagram, I would want to create a credentials account for Instagram in the application and the application generates a password for me to use when I sign up for Instagram.
+# As a user, I want to have the option of putting in a password that I want to use for the new credential account. For example, when creating my Instagram credential account, I want to have an option of putting in the password I want to use instead of having the application generate a password for me.
+# As a user, I also want to view my various account credentials and their passwords in the application.
+# As a user, I want to delete a credentials account that I no longer need in the application.
 def main():
 
     print("Hello,To proceed choose an option and type the command of any below.Thank you")
